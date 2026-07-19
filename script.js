e1a9778e46d868c9a93c59ec384ce37d7efd30fd
Mattaparthi Luhipriya <luhipriyamattaparthi@gmail.com>
2026-07-19 12:09:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+document.addEventListener("keydown", function(event){
+
+    if(!isNaN(event.key) || "+-*/.%".includes(event.key)){
+        appendValue(event.key);
+    }
+
+    if(event.key === "Enter"){
+        calculate();
+    }
+
+    if(event.key === "Backspace"){
+        deleteLast();
+    }
+
+    if(event.key === "Escape"){
+        clearDisplay();
+    }
+});
